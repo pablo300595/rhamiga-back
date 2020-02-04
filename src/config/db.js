af01819a345dea836b187ce1b08a78518412ec80
Pablo Valenzuela <jopavalenzuelami@ittepic.edu.mx>
2020-02-03 23:03:35 -0700

    feat: Remote database configuration foundations
    The app database has been configured in order to retrieve remote
    data.

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const URI = `mongodb://localhost/rhamiga`;
+// const URI = `mongodb://localhost/rhamiga`;
+const URI = `mongodb://admin:admin@cluster0-shard-00-00-6wnub.mongodb.net:27017,cluster0-shard-00-01-6wnub.mongodb.net:27017,cluster0-shard-00-02-6wnub.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`;
 mongoose.connect(URI,{ useNewUrlParser: true, useUnifiedTopology: true})
     .then(db => console.log('db is connected'))
     .catch(err => console.error(err));
