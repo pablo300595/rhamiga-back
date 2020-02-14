f6ea25f95d7f06773e0e70c2b1f98a90933ac1f5
Pablo Valenzuela <jopavalenzuelami@ittepic.edu.mx>
2020-02-14 13:00:53 -0700

    fix: Default Values in Candidate

--- src/models/candidate.model.js
+++ src/models/candidate.model.js
@@ -6,17 +6,17 @@ const CandidateSchema = new Schema({
     status: {type: String, required: true},
     resume: {type: Object, required: false, default: null},
     age: {type: Number, required: true},
-    email: {type: String, required: true},
-    phoneNumber: {type: String, required: true},
+    email: {type: String, required: true, default: null},
+    phoneNumber: {type: String, required: true, default: null},
     state: {type: String, required: true},
     city: {type: String, required: true},
     nationality: {type: String, required: true},
     certifications: {type: [Object], required: false},
     languages: {type:[Object], required: false},
     experience: {type: Number, required:false, default:0},
-    career: {type: String, required: true},
-    degree: {type: String, required: true},
-    category: {type: String, required: true},
+    career: {type: String, required: true, default: null},
+    degree: {type: String, required: true, default: null},
+    category: {type: String, required: true, default: null},
     sex: {type: String, required: true}
 }); 
 module.exports = mongoose.model('Candidate', CandidateSchema);
